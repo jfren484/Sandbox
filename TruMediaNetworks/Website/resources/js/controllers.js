be4673e9aa4dab4aa0de471f6fa55414ecbecbeb
jfren484 <jay@frenchfryz.com>
2015-12-31 11:28:35 -0500

    Highcharts with angular (switching gears next)

--- TruMediaNetworks/Website/resources/js/controllers.js
+++ TruMediaNetworks/Website/resources/js/controllers.js
@@ -1,6 +1,9 @@
-﻿var playerStatsApp = angular.module('playerStatsApp', []);
+﻿// ReSharper disable MissingHasOwnPropertyInForeach
+// ReSharper disable PossiblyUnassignedProperty
 
-playerStatsApp.controller('playerStatsController', function ($scope, $http) {
+var playerStatsApp = angular.module('playerStatsApp', ['highcharts-ng']);
+
+playerStatsApp.controller('playerStatsController', function($scope, $http) {
     $scope.players = [
         { 'id': 425783 },
         { 'id': 458731 },
@@ -9,42 +12,67 @@ playerStatsApp.controller('playerStatsController', function ($scope, $http) {
 
     $scope.players.forEach(function(player) {
         $http.get('mlb-' + player.id + '.json')
-             .success(function (data) {
-                 var totalAB = 0, totalH = 0, totalBB = 0, totalSF = 0, totalTB = 0;
-                 var totals = {};
+            .success(function(data) {
+                var totals = {};
 
-                 ['PA', 'AB', 'H', 'BB', 'HBP', 'SF', 'TB'].forEach(function (col) {
-                     totals[col] = 0;
-                 });
+                ['PA', 'AB', 'H', 'BB', 'HBP', 'SF', 'TB'].forEach(function(col) {
+                    totals[col] = 0;
+                });
 
-                 player.rows = data.rows.map(function (rowSource) {
-                     var rowDest = {};
+                player.rows = data.rows.map(function(rowSource) {
+                    var rowDest = {};
 
-                     data.header.forEach(function (col, colIndex) {
-                         rowDest[col.label] = rowSource[colIndex];
-                     });
+                    data.header.forEach(function(col, colIndex) {
+                        rowDest[col.label] = rowSource[colIndex];
+                    });
 
-                     rowDest.PA = rowDest.AB + rowDest.HBP + rowDest.BB + rowDest.SF;
+                    rowDest.PA = rowDest.AB + rowDest.HBP + rowDest.BB + rowDest.SF;
 
-                     for (var prop in totals) {
-                         totals[prop] += rowDest[prop];
-                         rowDest['total' + prop] = totals[prop];
-                     }
+                    for (var rowProp in totals) {
+                        totals[rowProp] += rowDest[rowProp];
+                        rowDest['total' + rowProp] = totals[rowProp];
+                    }
 
-                     rowDest.AVG = totals.AB ? totals.H / totals.AB : 0;
-                     rowDest.OBP = totals.PA ? (totals.H + totals.HBP + totals.BB) / totals.PA : 0;
-                     rowDest.SLG = totals.AB ? totals.TB / totals.AB : 0;
-                     rowDest.OPS = rowDest.OBP + rowDest.SLG;
+                    rowDest.AVG = totals.AB ? totals.H / totals.AB : 0;
+                    rowDest.OBP = totals.PA ? (totals.H + totals.HBP + totals.BB) / totals.PA : 0;
+                    rowDest.SLG = totals.AB ? totals.TB / totals.AB : 0;
+                    rowDest.OPS = rowDest.OBP + rowDest.SLG;
 
-                     return rowDest;
-                 });
+                    return rowDest;
+                });
 
-                 var lastRow = player.rows[player.rows.length - 1];
-                 for (var prop in lastRow) {
-                     player[prop] = lastRow[prop];
-                 }
-             });
+                var lastRow = player.rows[player.rows.length - 1];
+                for (var lastRowProp in lastRow) {
+                    player[lastRowProp] = lastRow[lastRowProp];
+                }
+            });
     });
 
     $scope.player = $scope.players[0];
+
+    $scope.lineChart = {
+        options: {
+            chart: {
+                type: 'line'
+            }
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        series: [
+            {
+                name: 'Batting Average (AVG)',
+                data: !$scope.player.rows ? null : $scope.player.rows.map(function(row) {
+                    return [row.gameDate, row.AVG];
+                })
+            }, {
+                name: 'On-base Plus Slugging (OPS)',
+                data: [10, 15, 12, 8, 7]
+            }
+        ],
+        title: {
+            text: 'Player Stats Over Time'
+        },
+        loading: false
+    };
 });
