613d42252d4d863f71ceff75bad5b923867806f9
Jay French <JFrenc@polarisind.com>
2019-02-27 16:26:11 -0600

    Jason's code

---
+++ CpS230/Lab5Random/Asm/Build.cmd
@@ -0,0 +1,2 @@
+%USERPROFILE%\AppData\Local\bin\NASM\nasm.exe -f win64 lab5.asm
+cl /Zi lab5.obj msvcrt.lib legacy_stdio_definitions.lib
--- CpS230/Lab5Random/Asm/lab5.asm
+++ CpS230/Lab5Random/Asm/lab5.asm
@@ -1,24 +1,71 @@
-.386
-.MODEL FLAT
-.STACK 4096
+default rel
 
-.data
-hello db 'Hello World!', 0ah, 0
+extern printf
+extern scanf
 
-.code
-INCLUDELIB LIBCMT
-EXTRN _printf:NEAR
+section .text
 
-PUBLIC _main
-_main PROC
+global main
+main:
+  push    rbp
+	mov     rbp, rsp
+	sub     rsp, 32
 
-   mov   eax, offset hello
-   push  eax
-   call  _printf
-   pop   ecx
+.while:
+  mov     rcx, str_guess_prompt
+  call    printf
 
-   mov   eax, 0
-   ret
+  mov     rdx, int_guess
+  mov     rcx, str_read_guess
+  call    scanf
 
-_main ENDP
-END
+  mov     rdx, [int_guess]
+  mov     rcx, [int_magic_number]
+  cmp     rdx, rcx
+  je      .end
+  jl      .guess_lower
+  jg      .guess_higher
+
+.guess_lower:
+  mov     rcx, str_too_low
+  call    printf
+
+  inc     qword [int_amount_of_guesses]
+
+  jmp     .while
+
+.guess_higher:
+  mov     rcx, str_too_high
+  call    printf
+
+  inc     qword [int_amount_of_guesses]
+
+  jmp     .while
+
+.end:
+  inc     qword [int_amount_of_guesses]
+
+  mov     rdx, qword [int_amount_of_guesses]
+  mov     rcx, str_amount_of_guesses
+
+  mov     rdx, qword [int_amount_of_guesses]
+  call    printf
+
+  mov     rcx, str_correct
+  call    printf
+
+  mov     rax, 0
+  pop	  	rbp
+  ret
+
+section .data
+  str_guess_prompt      db   "I'm thinking of a number between 1 and 100; what is it? ",13,10,0
+  str_amount_of_guesses db   "The amount of guesses you took was: %lld", 13,10,0
+  str_read_guess        db   "%lld", 0
+  str_correct           db   "You guessed it! Thanks for playing...",13,10,0
+  str_too_high          db   "Too high!", 13,10,0
+  str_too_low           db   "Too low!", 13,10,0
+
+  int_amount_of_guesses dq   0
+  int_guess             dq   0
+  int_magic_number      dq   72
