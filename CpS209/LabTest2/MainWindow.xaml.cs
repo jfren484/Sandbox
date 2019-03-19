6dac3ff9aa28b532186f0af9ac1728da6e2661e1
Jay French <JFrenc@polarisind.com>
2019-03-19 12:38:58 -0500

    Lab test 2

---
+++ CpS209/LabTest2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabTest2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel viewModel = new ViewModel();
+        private Label lblLookAtMe;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        private void BtnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(txtNumber.Text, out double val))
+            {
+                txtNumber.Text = "";
+                viewModel.CalculatedValue += val;
+            }
+            else
+            {
+                MessageBox.Show($"'{txtNumber.Text}' is not a valid number!");
+            }
+
+            txtNumber.Focus();
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            btnCreate.IsEnabled = false;
+
+            lblLookAtMe = new Label
+            {
+                Content = "Look at me",
+                Margin = new Thickness(0, gridMain.ActualHeight - lblTotal.ActualHeight, 0, 0)
+            };
+
+            gridMain.Children.Add(lblLookAtMe);
+        }
+    }
+}
---
+++ CpS209/LabTest2/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LabTest2
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private double _calculatedValue;
+        public double CalculatedValue
+        {
+            get => _calculatedValue;
+            set
+            {
+                _calculatedValue = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
