0df228176928f2751e2c8f8f86972cd9ba19342a
Jay French <jay@frenchfryz.com>
2019-09-28 20:49:55 -0500

    Updated files

--- CpS210/Lab3/square.h
+++ CpS210/Lab3/square.h
@@ -1,44 +1,44 @@
 #ifndef SQUARE_H
 #define SQUARE_H
+
 #include <iostream>
 
 template <typename T>
 class square {
 public:
-    const int N;
+	const int N;
 private:
-    T *cells;
+	T *cells;
 
-public: 
-    square(int n, bool zero=true) //n is sizeofsquare
-    N(n), //intializes const N with little n
-    cells(new T[n*n])
-    {
-        if (zero) {
-            for (int k = 0; k < n*n; cells[k] = 0) { ; }
-        }
-    }
+public:
+	square(int n, bool zero=true) : 
+		N(n), cells(new T[n*n]) 
+	{
+		if (zero) {
+			for (int k=0; k < n*n; cells[k++] = 0) { ; }
+		}
+	}
+	~square() { delete [] cells; }
 
-    ~square() { delete [] cells; }
-
-    T &at(int row, int col) const { 
-        return cells[N*row+col];
-    } 
-    
-    const T&at(int row, int col) const { //second const indicates the the function is not changing the object (cells)
-        return cells[N*row+col];
-    }
-
-    std::ostream &print(std::ostream &os) {
-        for (int row = 0; row < N; ++row) {
-            os << sep << at(row, 0);
-            for (int col = 1; col < N; ++col) {
-                os << sep << at(row,col);
-            }
-            os << std:endl;
-        }
-        return os;
-    }
-}; //end of class square
+	T &at(int row, int col) { 
+		return cells[N*row+col];
+	}
+	
+	const T& at(int row, int col) const { 
+		return cells[N*row+col];
+	}
+	
+	std::ostream &print(std::ostream &os, char sep=' ') {
+		for (int row=0; row < N; ++row) {
+			os << sep << at(row,0);
+			for (int col=1; col < N; ++col) {
+				os << sep << at(row,col);
+			}
+			os << std::endl;
+		}
+		return os;
+	}
+	
+}; // end of class square
 
 #endif
--- CpS210/Lab3/testSquare.cpp
+++ CpS210/Lab3/testSquare.cpp
@@ -1,14 +1,15 @@
 #include "square.h"
 
-int main() {
-    const int N=8;
-    square<float> aSquare(N);
 
-    int cnt = 1;
-    for (int row = 0; row < N; ++row) {
-        for (int col = 0; col < N; ++col) {
-            aSquare.at(row,col) = cnt++;
-        }
-    }
-    aSquare.print(std::cout);
+int main() {
+	const int N=8;
+	square<int> aSquare(N);
+	
+	int cnt = 1;
+	for (int row=0; row < N; ++row) {
+		for (int col=0; col < N; ++ col) {
+			aSquare.at(row,col) = cnt++;
+		}
+	}
+	aSquare.print(std::cout);
 }
