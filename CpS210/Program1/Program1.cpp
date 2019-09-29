cc5586ec2ad33b99fe3adba34f499e26024af3a2
Jay French <jay@frenchfryz.com>
2019-09-28 23:28:16 -0500

    Handle rows != cols

--- CpS210/Program1/Program1.cpp
+++ CpS210/Program1/Program1.cpp
@@ -1,11 +1,11 @@
 #include <cstdio>
 #include "generation.h"
 
-int N, K;
+int ROWS, COLS, K;
 
 void fillWithRandomValues(generation &g) {
-	for (int row = 0; row < N; ++row) {
-		for (int col = 0; col < N; ++col) {
+	for (int row = 0; row < ROWS; ++row) {
+		for (int col = 0; col < COLS; ++col) {
 			g.at(row, col) = (rand() % 2 == 1);
 		}
 	}
@@ -13,19 +13,26 @@ void fillWithRandomValues(generation &g) {
 
 int main(int argc, const char* argv[]) {
 	if (argc > 2) {
-		N = atoi(argv[1]);
-		K = atoi(argv[2]);
+		ROWS = atoi(argv[1]);
+		if (argc > 3) {
+			COLS = atoi(argv[2]);
+			K = atoi(argv[3]);
+		}
+		else {
+			COLS = ROWS;
+			K = atoi(argv[2]);
+		}
 	}
 
-	if (argc < 3 || N < 2 || K < 0) {
-		std::cout << "usage: program1 N K\n\tN >= 2: size of matrix\n\tK >= 0: number of generations to run";
+	if (argc < 3 || ROWS < 2 || COLS < 2 || K < 0) {
+		std::cout << "usage: program1 R [C] K\n\tR >= 2: number of rows in matrix\n\tC >= 2: if specified, number of columns in matrix; otherwise, C = R\n\tK >= 0: number of generations to run";
 		return 0;
 	}
 
-	printf("Running %d generations for a matrix of size %dx%d\n", K, N, N);
+	printf("Running %d generations for a matrix of size %dx%d\n", K, ROWS, COLS);
 
 	int curIndex = 0;
-	generation generations[2]{ {N}, {N} };
+	generation generations[2]{ {ROWS, COLS}, {ROWS, COLS} };
 	fillWithRandomValues(generations[0]);
 
 	for (int k = 0; k < K; ++k, curIndex = 1 - curIndex) {
@@ -33,11 +40,12 @@ int main(int argc, const char* argv[]) {
 		generation& next = generations[1 - curIndex];
 
 		// Print the current state
+		printf("Generation %d\n", k);
 		curr.print(std::cout);
 
 		// Compute the next generation
-		for (int row = 0; row < N; ++row) {
-			for (int col = 0; col < N; ++col) {
+		for (int row = 0; row < ROWS; ++row) {
+			for (int col = 0; col < COLS; ++col) {
 				int neighbors = curr.countNeighbors(row, col);
 
 				if (curr.at(row, col)) {
@@ -65,5 +73,6 @@ int main(int argc, const char* argv[]) {
 	}
 
 	// Print the current state
+	printf("Generation %d\n", K);
 	generations[curIndex].print(std::cout);
 }
--- CpS210/Program1/generation.h
+++ CpS210/Program1/generation.h
@@ -5,40 +5,40 @@
 
 class generation {
 public:
-	const int N;
+	const int COLS, ROWS;
 
 private:
 	bool* cells;
 
-	const char* cellDisplayValue(int row, int col) {
-		return at(row, col) ? "X" : " ";
+	void printCell(std::ostream& os, int row, int col) {
+		os << at(row, col) ? "X" : " ";
 	}
 
 	void printRowSeperator(std::ostream& os) {
-		for (int col = 0; col < N; ++col) {
+		for (int col = 0; col < COLS; ++col) {
 			os << "+-";
 		}
 		os << "+" << std::endl;
 	}
 
 public:
-	generation(int n) : N(n), cells(new bool[n * n]) {	}
+	generation(int rows, int cols) : ROWS(rows), COLS(cols), cells(new bool[rows * cols]) {	}
 	~generation() { delete[] cells; }
 
 	bool& at(int row, int col) {
-		return cells[N * row + col];
+		return cells[COLS * row + col];
 	}
 
 	int countNeighbors(int row, int col) {
-		int minRow = row > 0 ? row - 1 : row;
-		int maxRow = row < N - 1 ? row + 1 : row;
-		int minCol = col > 0 ? col - 1 : col;
-		int maxCol = col < N - 1 ? col + 1 : col;
+		int startRow = row > 0 ? row - 1 : row;
+		int endRow = row < ROWS - 1 ? row + 1 : row;
+		int startCol = col > 0 ? col - 1 : col;
+		int endCol = col < COLS - 1 ? col + 1 : col;
 
 		int count = 0;
 
-		for (int r = minRow; r <= maxRow; ++r) {
-			for (int c = minCol; c <= maxCol; ++c) {
+		for (int r = startRow; r <= endRow; ++r) {
+			for (int c = startCol; c <= endCol; ++c) {
 				if ((r != row || c != col) && at(r, c)) {
 					++count;
 				}
@@ -49,15 +49,16 @@ public:
 	}
 
 	const bool& at(int row, int col) const {
-		return cells[N * row + col];
+		return cells[COLS * row + col];
 	}
 
 	std::ostream& print(std::ostream& os) {
-		for (int row = 0; row < N; ++row) {
+		for (int row = 0; row < ROWS; ++row) {
 			printRowSeperator(os);
 
-			for (int col = 0; col < N; ++col) {
-				os << "|" << cellDisplayValue(row, col);
+			for (int col = 0; col < COLS; ++col) {
+				os << "|";
+				printCell(os, row, col);
 			}
 			os << "|" << std::endl;
 		}
