76e5f6414f95c14824d439cc3730df5ecd81dc60
Jay French <jay@frenchfryz.com>
2019-10-21 19:38:58 -0500

    Added Stack

---
+++ CpS210/Lab6/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab6
+{
+    public class Stack<T>
+    {
+        private List<T> _vals = new List<T>();
+
+        public bool Empty()
+        {
+            return !_vals.Any();
+        }
+
+        public void Push(T value)
+        {
+            _vals.Add(value);
+        }
+
+        public T Pop()
+        {
+            T value = _vals.Last();
+            _vals.RemoveAt(_vals.Count - 1);
+            return value;
+        }
+
+        public T Top()
+        {
+            return _vals.Last();
+        }
+    }
+}
