77f97bee38659481ca3f804bc76c710fea084d26
Jay French <jay@frenchfryz.com>
2019-11-04 22:40:17 -0600

    Longest run working

--- CpS210/Lab7/Lab7SnowRun/Lab7SnowRun.cpp
+++ CpS210/Lab7/Lab7SnowRun/Lab7SnowRun.cpp
@@ -1,11 +1,27 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
+#include <stack>
 #include <string>
 #include <sstream>
 #include <vector>
 #include "square.h"
 
 using namespace std;
+
+struct point {
+	int Row, Col;
+
+	point() : Row(0), Col(0) {}
+};
+
+struct grid_data {
+	point Location, FromLocation;
+	int Value;
+	int Steps;
+
+	grid_data() : Location(), FromLocation(), Value(0), Steps(0) {}
+};
 
 vector<string> split(const string& s, char delimiter)
 {
@@ -19,6 +35,59 @@ vector<string> split(const string& s, char delimiter)
 	}
 
 	return tokens;
+}
+
+int calculateLongestRun(square<grid_data>* grid) {
+	queue<point> todoQueue;
+
+	for (int row = 0; row < grid->ROWS; ++row) {
+		for (int col = 0; col < grid->COLS; ++col) {
+			todoQueue.push(grid->at(row, col).Location);
+		}
+	}
+
+	vector<point> directions(4);
+	directions[0].Row = -1;
+	directions[1].Row = 1;
+	directions[2].Col = -1;
+	directions[3].Col = 1;
+
+	for (; !todoQueue.empty(); todoQueue.pop()) {
+		point coords = todoQueue.front();
+
+		for (int i = 0; i < 4; ++i) {
+			point neighborCoords = coords;
+			neighborCoords.Row += directions[i].Row;
+			neighborCoords.Col += directions[i].Col;
+
+			if (neighborCoords.Row < 0 || neighborCoords.Row >= grid->ROWS || neighborCoords.Col < 0 || neighborCoords.Col >= grid->COLS) {
+				continue;
+			}
+
+			// Neighbor value must be lower to move there.
+			if (grid->at(neighborCoords.Row, neighborCoords.Col).Value >= grid->at(coords.Row, coords.Col).Value) {
+				continue;
+			}
+
+			int steps = grid->at(coords.Row, coords.Col).Steps + 1;
+			if (steps > grid->at(neighborCoords.Row, neighborCoords.Col).Steps) {
+				todoQueue.push(neighborCoords);
+				grid->at(neighborCoords.Row, neighborCoords.Col).FromLocation = coords;
+				grid->at(neighborCoords.Row, neighborCoords.Col).Steps = steps;
+			}
+		}
+	}
+
+	priority_queue<int> steps;
+	for (int row = 0; row < grid->ROWS; ++row) {
+		for (int col = 0; col < grid->COLS; ++col) {
+			steps.push(grid->at(row, col).Steps);
+		}
+	}
+
+	int longest = steps.top();
+
+	return longest + 1;
 }
 
 int main()
@@ -37,22 +106,23 @@ int main()
 		rows = stoi(parts[1]);
 		cols = stoi(parts[2]);
 
-		square<int> grid(rows, cols);
+		square<grid_data> grid(rows, cols);
 
 		for (int row = 0; row < rows; ++row) {
 			getline(cin, line);
 			parts = split(line, ' ');
 
 			for (int col = 0; col < cols; ++col) {
-				grid.at(row, col) = stoi(parts[col]);
+				grid.at(row, col).Location.Row = row;
+				grid.at(row, col).Location.Col = col;
+				grid.at(row, col).Value = stoi(parts[col]);
 			}
 		}
 
-		cout << "Test case: " << name << " (" << rows << ", " << cols << ")" << endl;
-		grid.print(cout);
-	}
+		int longestRun = calculateLongestRun(&grid);
 
-	cout << line << endl;
+		cout << name << ": " << longestRun << endl;
+	}
 
 	return 0;
 }
