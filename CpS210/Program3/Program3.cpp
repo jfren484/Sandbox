73fd872f39ed945e813ad24529aba032a8ae1881
Jay French <jay@frenchfryz.com>
2019-11-20 18:43:21 -0600

    Implemented method 2

--- CpS210/Program3/Program3.cpp
+++ CpS210/Program3/Program3.cpp
@@ -10,59 +10,19 @@
 using namespace std;
 using namespace std::chrono;
 
-//struct WordFreq {
-//	string word;
-//	int freq;
-//	WordFreq(string w = "", int f = 0) : word(w), freq(f) { ; }
-//};
-//bool operator <(const WordFreq& wf1, const WordFreq& wf2) {
-//	return (wf1.word < wf2.word);
-//}
-
-//int main() {
-//	int freq;
-//	string word;
-//	vector<WordFreq> wf;
-//	while (cin >> word >> freq) {
-//		wf.push_back({ word,freq });
-//	}
-//	sort(wf.begin(), wf.end());
-//	for (auto pr : wf) {
-//		cout << '(' << pr.word << ',' << pr.freq << ")\n";
-//	}
-//	return 0;
-//}
-
-int main(int argc, const char* argv[])
-{
-	if (argc < 2) {
-		cout << "Specify input file on command line" << endl;
-		return 1;
-	}
-
-	ifstream in(argv[1]);
-	if (!in.is_open()) {
-		cout << "Could not read file " << argv[1] << endl;
-		return 1;
-	}
-
-	bool debug = argc >= 3 && _stricmp(argv[2], "DEBUG") == 0;
-
+void method1(ifstream* in) {
 	string word;
 	int freq;
 	vector<string> words;
 	map<string, int> wordFreqs;
 
-	high_resolution_clock::time_point t1 = high_resolution_clock::now();
-
-	while (in >> word >> freq) {
+	while (*in >> word >> freq) {
 		words.push_back(word);
 		wordFreqs[word] = freq;
 	}
 	sort(words.begin(), words.end());
 	int wordCount = words.size();
 
-	// Rows = wordCount, Cols = wordCount + 1
 	vector<vector<int>> costs(wordCount + 1, vector<int>(wordCount + 1));
 	vector<vector<int>> roots(wordCount, vector<int>(wordCount + 1));
 
@@ -91,12 +51,111 @@ int main(int argc, const char* argv[])
 		}
 	}
 
-	if (debug) {
-		high_resolution_clock::time_point t2 = high_resolution_clock::now();
-		duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+	cout << words[roots[0][wordCount]] << " " << costs[0][wordCount] << endl;
+}
 
-		cout << "Time taken for " << words.size() << " values: " << time_span.count() << " seconds." << endl;
+struct WordFreq {
+	string word;
+	int freq;
+	WordFreq(string w = "", int f = 0) : word(w), freq(f) { ; }
+};
+
+bool operator <(const WordFreq& wf1, const WordFreq& wf2) {
+	return (wf1.word < wf2.word);
+}
+
+void method2(ifstream* in) {
+	int freq;
+	string word;
+	vector<WordFreq> wordFreqs;
+
+	while (*in >> word >> freq) {
+		wordFreqs.push_back({ word, freq });
+	}
+	sort(wordFreqs.begin(), wordFreqs.end());
+
+	int wordCount = wordFreqs.size();
+
+	vector<vector<int>> costs(wordCount + 1, vector<int>(wordCount + 1));
+	vector<vector<int>> roots(wordCount, vector<int>(wordCount + 1));
+
+	for (int i = 0; i < wordCount; ++i) {
+		costs[i][i + 1] = wordFreqs[i].freq;
 	}
 
-	cout << words[roots[0][wordCount]] << " " << costs[0][wordCount] << endl;
+	for (int d = 0; d < wordCount - 1; ++d) {
+		for (int e = 0; e < wordCount - d - 1; ++e) {
+			int row = e;
+			int col = e + d + 2;
+
+			costs[row][col] = INT32_MAX;
+
+			for (int k = 0; k < d + 2; ++k) {
+				int cost = costs[row][col - d - 2 + k] + costs[row + 1 + k][col];
+				if (cost < costs[row][col]) {
+					costs[row][col] = cost;
+					roots[row][col] = row + k;
+				}
+			}
+
+			for (int x = row; x < row + d + 2; ++x) {
+				costs[row][col] += wordFreqs[x].freq;
+			}
+		}
+	}
+
+	cout << wordFreqs[roots[0][wordCount]].word << " " << costs[0][wordCount] << endl;
+}
+
+int main(int argc, const char* argv[])
+{
+	if (argc < 3) {
+		cout << "Usage: Program3 <file> <RUN1 | RUN2 | RUNBOTH>" << endl;
+		return 1;
+	}
+
+	ifstream in(argv[1]);
+	if (!in.is_open()) {
+		cout << "Could not read file " << argv[1] << endl;
+		return 1;
+	}
+
+	bool runMethod1 = _stricmp(argv[2], "RUN1") == 0 || _stricmp(argv[2], "RUNBOTH") == 0;
+	bool runMethod2 = _stricmp(argv[2], "RUN2") == 0 || _stricmp(argv[2], "RUNBOTH") == 0;
+
+	high_resolution_clock::time_point t1, t2;
+	duration<double> time_span;
+
+	if (runMethod1) {
+		cout << "Running method 1 (using map<string, int> and vector<string>)" << endl;
+
+		t1 = high_resolution_clock::now();
+
+		method1(&in);
+
+		t2 = high_resolution_clock::now();
+		time_span = duration_cast<duration<double>>(t2 - t1);
+
+		cout << "Time taken for method 1: " << time_span.count() << " seconds." << endl;
+
+		if (runMethod2) {
+			in.clear();
+			in.seekg(0, ios::beg);
+		}
+	}
+
+	if (runMethod2) {
+		cout << "Running method 2 (using vector<WordFreq>)" << endl;
+
+		t1 = high_resolution_clock::now();
+
+		method2(&in);
+
+		t2 = high_resolution_clock::now();
+		time_span = duration_cast<duration<double>>(t2 - t1);
+
+		cout << "Time taken for method 2: " << time_span.count() << " seconds." << endl;
+	}
+
+	in.close();
 }
