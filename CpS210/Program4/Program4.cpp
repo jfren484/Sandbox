0a2e47fecf694e1be8a05019ba56ea63b532081c
Jay French <jay@frenchfryz.com>
2019-12-05 22:34:21 -0600

    Test data and AdjList

--- CpS210/Program4/Program4.cpp
+++ CpS210/Program4/Program4.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+#include "adjlist.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a, b, weight;
+	vector<int> weights;
+	while (cin >> a >> b >> weight) {
+		weights.push_back(weight);
+	}
+
+	for (unsigned int i = 0; i < weights.size(); ++i) {
+		cout << weights[i] << endl;
+	}
 }
