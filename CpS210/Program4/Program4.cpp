abbabb5a0ad830a009e2b930c2c879494d95d65d
Jay French <jay@frenchfryz.com>
2019-12-07 22:19:35 -0600

    Submitted version

--- CpS210/Program4/Program4.cpp
+++ CpS210/Program4/Program4.cpp
@@ -5,16 +5,22 @@ using namespace std;
 
 int main()
 {
-	AdjList<int> adjlist;
+	AdjList<unsigned int> adjlist;
 
 	int a, b, weight;
 	while (cin >> a >> b >> weight) {
 		adjlist.addEdge(a, b, weight);
 	}
 
-	vector<unsigned int> foundBy;
-	vector<int> dist;
-	adjlist.dijkstra(0, foundBy, dist, 0);
+	AdjList<unsigned int>::DisjointSet dj(adjlist.size(), adjlist.size() - 1);
 
-	//cout << weights[i] << endl;
+	adjlist.kruskals(dj);
+
+	weight = 0;
+	for (int i = 0; i < dj.size(); i++) {
+		cout << dj[i].v1 << " " << dj[i].v2 << endl;
+		weight += dj[i].w;
+	}
+
+	cout << weight << endl;
 }
--- CpS210/Program4/adjlist.h
+++ CpS210/Program4/adjlist.h
@@ -12,97 +12,127 @@ public:
 		Vertex v;
 		Weight w;
 		VertexWeight(Vertex _v, Weight _w) : v(_v), w(_w) { ; }
-		bool operator >(VertexWeight rhs) const { return (w > rhs.w); }			
+		bool operator >(VertexWeight rhs) const { return (w > rhs.w); }
+	};
+	struct Edge {
+		Vertex v1, v2;
+		Weight w;
+		//Edge(Vertex _v1, Vertex _v2, Weight _w) : v1(_v1), v2(_v2), w(_w) { ; }
+		bool operator >(Edge rhs) const { return (w > rhs.w); }
+	};
+	class DisjointSet {
+	public:
+		int numVertices, numEdges;
+	private:
+		Edge* edges;
+		int counter;
+	public:
+		DisjointSet(int _numVertices, int _numEdges): numVertices(_numVertices), numEdges(_numEdges), counter(0) {
+			edges = new Edge[_numEdges * sizeof(Edge)];
+		}
+		~DisjointSet() {
+			delete[] edges;
+		}
+
+		int size() {
+			return counter;
+		}
+
+		Edge& operator[] (int i) {
+			return edges[i];
+		}
+
+		void addEdge(Edge e) {
+			edges[counter].v1 = e.v1;
+			edges[counter].v2 = e.v2;
+			edges[counter++].w = e.w;
+		}
+
+		void removeLast() {
+			--counter;
+		}
+
+		int find(int subsets[], int i)
+		{
+			if (subsets[i] == -1)
+				return i;
+			return find(subsets, subsets[i]);
+		}
+
+		void Union(int subsets[], int srcRoot, int destRoot)
+		{
+			int srcSet = find(subsets, srcRoot);
+			int destSet = find(subsets, destRoot);
+			if (srcSet != destSet)
+			{
+				subsets[srcSet] = destSet;
+			}
+		}
+
+		int hasLoop()
+		{
+			int* subsets = new int[numVertices * sizeof(int)];
+
+			memset(subsets, -1, sizeof(int) * numVertices);
+
+			for (int i = 0; i < counter; ++i)
+			{
+				int srcRoot = find(subsets, edges[i].v1);
+				int destRoot = find(subsets, edges[i].v2);
+
+				if (srcRoot == destRoot)
+					return 1;
+
+				Union(subsets, srcRoot, destRoot);
+			}
+			return 0;
+		}
+	};
+
+	class CompareEdges
+	{
+	public:
+		bool operator() (Edge a, Edge b)
+		{
+			return a.w > b.w;
+		}
 	};
 private:
-	std::vector<std::vector<VertexWeight>> adj;
+	std::vector<std::vector<VertexWeight> > adj;
+	std::priority_queue<Edge, std::vector<Edge>, CompareEdges> edges;
 	unsigned int nEdges;
 public:
-	AdjList(): nEdges(0) { ; }
+	AdjList(): nEdges(0) {; }
 	
+	int size() {
+		return adj.size();
+	}
+
 	void addVertex(Vertex u) { 
 		// u+1 needs to be <= size
 		if (adj.size() <= u) { adj.resize(u+1); }
 	}
 	
 	void addEdge(Vertex src, Vertex dest, Weight w) {
-		// make sure both src and dest are valid indices
 		addVertex(src < dest ? dest : src);
 		
 		adj[src].push_back({dest, w});
 		if (Undirected) {
 			adj[dest].push_back({src, w});
 		}
+		edges.push({src, dest, w});
 		++nEdges;
 	}
-	
-	void dijkstra(Vertex start, 
-		std::vector<Vertex> &foundBy, // which vertex found ...
-		std::vector<Weight> &dist, Weight NoEdge) {
-		std::vector<bool> processed(adj.size(), false);
-		std::priority_queue<VertexWeight, std::vector<VertexWeight>, 
-			std::greater<VertexWeight>> vertices;
+
+	void kruskals(DisjointSet &dj) {
 		
-		// Clear and resize since we do not know what is in them		
-		foundBy.clear(); foundBy.resize(adj.size());
-		dist.clear(); dist.resize(adj.size(), NoEdge);
-		dist[start] = 0;
-		
-		unsigned int nProcessed = 0;
-		vertices.push({start,0});
-		while (!vertices.empty() && nProcessed < adj.size()) {
-			VertexWeight currPair = vertices.top();
-			vertices.pop();
-			if (processed[currPair.v]) { continue; }
-			
-			++nProcessed;
-			processed[currPair.v] = true;
-			//dist[currPair.v] = currPair.w; // now we know its distance
-			
-			for (VertexWeight edge : adj[currPair.v]) {
-				if (processed[edge.v]) { continue; }
-				Weight newDist = edge.w+currPair.w;
-				// update distances
-				if (NoEdge == dist[edge.v] || newDist < dist[edge.v]) {
-					dist[edge.v] = newDist;
-					foundBy[edge.v] = currPair.v;
-					vertices.push({edge.v, newDist});
-				}
-			}
-		}
-	}
-	
-	void prims(Vertex start, 
-		std::vector<Vertex> &foundBy, // which vertex found ...
-		std::vector<Weight> &dist, Weight NoEdge) {
-		std::vector<bool> processed(adj.size(), false);
-		std::priority_queue<VertexWeight, std::vector<VertexWeight>, 
-			std::greater<VertexWeight>> vertices;
-		
-		// Clear and resize since we do not know what is in them		
-		foundBy.clear(); foundBy.resize(adj.size());
-		dist.clear(); dist.resize(adj.size(), NoEdge);
-		
-		unsigned int nProcessed = 0;
-		vertices.push({start,0});
-		while (!vertices.empty() && nProcessed < adj.size()) {
-			VertexWeight currPair = vertices.top();
-			vertices.pop();
-			if (processed[currPair.v]) { continue; }
-			
-			++nProcessed;
-			processed[currPair.v] = true;
-			//dist[currPair.v] = currPair.w; // now we know its distance
-			
-			for (VertexWeight edge : adj[currPair.v]) {
-				if (processed[edge.v]) { continue; }
-				Weight newDist = edge.w;
-				// update distances
-				if (NoEdge == dist[edge.v] || newDist < dist[edge.v]) {
-					dist[edge.v] = newDist;
-					foundBy[edge.v] = currPair.v;
-					vertices.push({edge.v, newDist});
-				}
+		while (dj.size() < adj.size()-1) {
+			Edge currSmallest = edges.top();
+			edges.pop();
+
+			dj.addEdge(currSmallest);
+			if (dj.hasLoop()) {
+				dj.removeLast();
 			}
 		}
 	}
